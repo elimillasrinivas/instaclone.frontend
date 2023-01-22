129203bdb98b53b2acc6b62bf8a94ac86f8a29a8
Srinivas-Elimilla <elimilla@gmail.com>
2023-01-22 08:56:59 +0530

    initial commit

---
+++ src/components/PostView/postview.js
@@ -0,0 +1,30 @@
+import React,{useEffect, useState} from 'react'
+import Postcard from '../../postcard';
+import Header from '../Header/header';
+import './postview.css'
+const PostView=()=>{
+  const [posts,setposts]=useState([]);
+  useEffect(()=>{
+      fetch("http://localhost:5000/all").then(res=>res.json()).then(data=>{
+        setposts(data)
+    }).catch(err=>
+        console.log(err)
+      );
+  },[])
+
+  return (
+    <>
+      <div className='postview'>
+        <Header />
+        <div className="card-container">
+          {posts?.result
+            ?.map?.((post, _id) => {
+              return <Postcard post={post} key={post._id} />;
+            })
+            .reverse()}
+        </div>
+      </div>
+    </>
+  );
+}
+export default PostView
---
+++ src/postcard.js
@@ -0,0 +1,34 @@
+import './postcard.css'
+const Postcard=({post})=>{
+    return (
+      <>
+        <section className="card">
+          <section className="card-header">
+            <div>
+              <div className="card-header-name">{post.name}</div>
+              <div className="card-header-place">{post.location}</div>
+            </div>
+            <span className="fa fa-ellipsis-h" aria-hidden="true"></span>
+          </section>
+          <section className="card-image">
+            <img width={400}
+              src={`http://localhost:5000/image/${post.file_name}`}
+              alt="pictures"
+            />
+          </section>
+          <section className="card-actions">
+            <span>
+              <i className="fa fa-heart-o" aria-hidden="true"></i>
+            </span>
+            <span>
+              <i className="fa fa-paper-plane-o" aria-hidden="true"></i>
+            </span>
+            <span>{post.date.slice(0,15)}</span>
+          </section>
+          <section className="likes">{post.likes} Likes</section>
+          <section className="description">{post.description}</section>
+        </section>
+      </>
+    );
+}
+export default Postcard
